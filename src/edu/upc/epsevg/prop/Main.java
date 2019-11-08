a554281fba212343312c656b2eb24798ff398245
Pere <pere.montpeo@decathlon.com>
2019-11-08 09:02:04 +0100

    no infomada solucio

--- src/edu/upc/epsevg/prop/Main.java
+++ src/edu/upc/epsevg/prop/Main.java
@@ -1,12 +1,8 @@
 package edu.upc.epsevg.prop;
 
-import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.algorithms.layout.TreeLayout;
 import edu.uci.ics.jung.graph.DelegateTree;
-import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
-import edu.uci.ics.jung.visualization.VisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
@@ -19,13 +15,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.io.*;
-import java.util.Collection;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 /**
  *
@@ -33,102 +25,201 @@ import javax.swing.JScrollPane;
  */
 public class Main {
 
-    // final de la llista amplada
-    // prinicpi de la llista profunidtat
-    // descartar la branca 100% explorada de memoria
-    
-    public static void main(String[] args) {
+    private enum TipusCerca {
+        BREADTH_FIRST,
+        DEPTH_FIRST,
+        IDS
+    }
 
-        NinePuzzle.resetIds();
-        //----------------------------------------------------------------------------------------------
+    public static void main(String[] args) {
         // puzzle amb solució de 5 moviments
-        NinePuzzle start = new NinePuzzle( new int[]{1, 2, 3, 4, 5, 0, 6, 7, 8});
+        NinePuzzle start = new NinePuzzle(new int[]{1, 2, 3, 4, 5, 0, 6, 7, 8});
         NinePuzzle goal = new NinePuzzle(new int[]{1, 3, 5, 4, 2, 8, 6, 7, 0});
-        //----------------------------------------------------------------------------------------------
+
         // puzzle amb solució de 31 moviments
         //NinePuzzle start = new NinePuzzle(new int[]{8, 6, 7, 2, 5, 4, 3, 0, 1}); 
         //NinePuzzle goal = new NinePuzzle(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 0});
-
-        DelegateTree<NinePuzzle, Integer> g = new DelegateTree<>();
-        g.addVertex(start);
-        //g.addChild(1,start, goal);
-        bfs(g, start, goal);
-            
-        showGraph(g);
+        
+        //cercaNoInformada(start, goal, TipusCerca.BREADTH_FIRST, 32);
+        //cercaNoInformada(start, goal, TipusCerca.DEPTH_FIRST, 32);
+        cercaNoInformada(start, goal, TipusCerca.IDS, 32);
     }
-    
-    private static void bfs(DelegateTree<NinePuzzle, Integer> graph, NinePuzzle start, NinePuzzle goal) {
-        ArrayList<NinePuzzle> LNT = new ArrayList<>();
-        ArrayList<NinePuzzle> LNO = new ArrayList<>();
-        ArrayList<NinePuzzle> LF = new ArrayList<>();
-        NinePuzzle actual;
-        LNO.add(start);
-        boolean solucio = false;
-        while (!LNO.isEmpty() && !solucio) {
-            actual = LNO.get(0);
-            LNO.remove(0);
-            LNT.add(actual);
-            System.out.println("es solucio "+actual.isSolucio(goal));
-            if (actual.isSolucio(goal)) {
-                solucio = true;
-            } else {
-                LF = actual.expand();
-                for (NinePuzzle node : LF) {
-                    graph.addChild(node.getId(), actual, node);
+
+    private static void cercaNoInformada(NinePuzzle start , NinePuzzle goal, TipusCerca tipusCerca, int max_depth) {
+
+        boolean solutionReached = false;
+        int level = 1;
+        NinePuzzle solution = null;
+        DelegateTree<NinePuzzle, Integer> g = null;
+        do {
+            NinePuzzle.resetIds();
+            
+            g = new DelegateTree<>();
+
+            int e = 0;
+            List<NinePuzzle> LNO = new ArrayList<>();
+            LNO.add(start);
+            g.addVertex(start);
+
+            //solution = null;
+            while (!solutionReached && LNO.size() > 0) {
+                
+                // Prenem el primer valor de la LNO ( i el treiem de la llista )
+                NinePuzzle currentPuzzle = LNO.remove(0);
+
+                // Per les cerques en profunditat, ens hem d'assegurar
+                // d'anar netejant de la memòria les regions de l'arbre ja explorades
+                // i que no són solució.
+                if (tipusCerca != TipusCerca.BREADTH_FIRST) {
+                    NinePuzzle p = g.getParent(currentPuzzle);
+                    if (p != null) {
+                        int pe = g.getParentEdge(currentPuzzle);                        
+
+                        // Aquí buscarem la llista de nodes germans, i filtrem 
+                        // aquells amb un edge menor (i per tant, anteriors a l'actual i ja processats)
+                        // Aquests vèrtex es posen a una llista per ser eliminats posteriorment.
+                        ArrayList<Integer> edges = new ArrayList<>();
+                        for (Integer ced : g.getChildEdges(p)) {
+                            if (ced < pe) {
+                                edges.add(ced);
+                            }
+                        }
+                        // Esborrem els vèrtexs germans ja processats.
+                        for (Integer i : edges) {
+                            g.removeVertex(g.getOpposite(p, i));
+                        }
+                    }
                 }
-                LNO.addAll(LF);
-                // LF = expandir Na
-                // LNO afegir_ordenadament(LNO,LF)
-                // addAll afegeix la llista paràmetre al final
+                // Demanem la llista de moviments vàlids des de l'estat actual
+                List<Dir> dirs = currentPuzzle.validMoves();
+
+                int pos = 0;
+                // Per cadascun dels moviments possibles
+                for (Dir d : dirs) {
+
+                    // Repliquem el node ( és una còpia!)
+                    NinePuzzle f = currentPuzzle.clone();
+                    // sobre la còpia, fem el moviment.
+                    f.move(d);
+                    // només processem el fill si no és un cicle, i en el cas de cerca en profunditat, si no superem el límit de profunditat
+                    if (    !isCycle(g, f, currentPuzzle) && 
+                            (tipusCerca != TipusCerca.IDS || f.getDepth() < level)) {
+                        // afegim el node al graf
+                        g.addChild(e++, currentPuzzle, f);
+                        // mirem si el node és solució, i en cas positiu desem la solució i parem la iteració.
+                        if (f.isSolution(goal)) {
+                            solutionReached = true;
+                            solution = f;
+                            break;
+                        } else {
+                            // si no és solució, afegim el node a la LNO.                            
+                            if (tipusCerca == TipusCerca.BREADTH_FIRST) {
+                                // A la cerca en amplada, l'afegim al final.
+                                LNO.add(f);
+                            } else {
+                                // A la cerca en profunditat, l'afegim al començament.
+                                LNO.add(pos++, f);
+                            }
+                        }
+                    }
+                }
+            }
+        } while (tipusCerca == TipusCerca.IDS && !solutionReached && ++level < max_depth);
+        //----------------------------
+        if (!solutionReached) {
+            System.out.println("Sense solució.");
+        } else {
+
+            // Dibuixem el graf
+            showGraph(g);
+            // Fem un recorregut de la fulla solució fins l'arrel de l'arbre, i anem desant els nodes a una pila.
+            // La solució surt de desenpilar els nodes.
+            System.out.println("====================");
+            System.out.println("  Solution");
+            System.out.println("====================");
+            NinePuzzle v = solution;
+            ArrayList<NinePuzzle> nodesSolucio = new ArrayList<>();
+
+            while (v != null) {
+                nodesSolucio.add(0, v);
+                //System.out.println("*>"+v.toString());
+                v = g.getParent(v);
+            }
+            System.out.println("Mida: " + nodesSolucio.size());
+            int i = 0;
+            for (NinePuzzle np : nodesSolucio) {
+                System.out.println("(" + (i++) + ")" + np.toString());
             }
         }
     }
-    
-    private static void showGraph(DelegateTree<NinePuzzle, Integer> g){
 
-            Layout<Integer, String> layout = new TreeLayout(g, 70, 120);
-            VisualizationViewer<Integer, String> vv = new VisualizationViewer<Integer, String>(layout, new Dimension(500, 400));
-            vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
-            vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+    private static boolean isCycle(DelegateTree<NinePuzzle, Integer> g, NinePuzzle f, NinePuzzle parent) {
 
-            JFrame frame = new JFrame("Solution graph");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            final GraphZoomScrollPane scrollPane = new GraphZoomScrollPane(vv);
-            frame.getContentPane().add(scrollPane);
-
-            final ScalingControl scaler = new CrossoverScalingControl();
-
-            JButton plus = new JButton("+");
-            plus.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    scaler.scale(vv, 1.1f, vv.getCenter());
-                }
-            });
-            JButton minus = new JButton("-");
-            minus.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    scaler.scale(vv, 1 / 1.1f, vv.getCenter());
-                }
-            });
-
-            JButton reset = new JButton("reset");
-            reset.addActionListener(new ActionListener() {
-
-                public void actionPerformed(ActionEvent e) {
-                    vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT).setToIdentity();
-                    vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).setToIdentity();
-                }
-            });
-
-            JPanel controls = new JPanel();
-            controls.add(plus);
-            controls.add(minus);
-            controls.add(reset);
-            frame.getContentPane().add(controls, BorderLayout.SOUTH);
-
-            frame.pack();
-            frame.setVisible(true);
-        
+        while (parent != null) {
+            if (f.isSolution(parent)) {
+                return true;
+            }
+            parent = g.getParent(parent);
+        }
+        return false;
     }
+
+    private static void showGraph(DelegateTree<NinePuzzle, Integer> g) {
+
+        /*
+             System.out.println("https://dreampuf.github.io/GraphvizOnline");
+             System.out.println("http://graphviz.it/");
+             GraphExporter<NinePuzzle, DefaultEdge> exporter =
+             new DOTExporter<>(vertexIdProvider, vertexLabelProvider, null);
+             Writer writer = new StringWriter();
+             exporter.exportGraph(g, writer);
+             System.out.println(writer.toString());*/
+        Layout<Integer, String> layout = new TreeLayout(g, 70, 120);
+        // layout.setSize(new Dimension(300,300)); // sets the initial size of the space     // The BasicVisualizationServer<V,E> is parameterized by the edge types     
+        VisualizationViewer<Integer, String> vv = new VisualizationViewer<Integer, String>(layout, new Dimension(500, 400));
+        //vv.setPreferredSize(); //Sets the viewing area size    
+        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+
+        JFrame frame = new JFrame("Simple Graph View");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final GraphZoomScrollPane scrollPane = new GraphZoomScrollPane(vv);
+        //JScrollPane scrollPane = new JScrollPane(vv);
+        frame.getContentPane().add(scrollPane);
+
+        final ScalingControl scaler = new CrossoverScalingControl();
+
+        JButton plus = new JButton("+");
+        plus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1.1f, vv.getCenter());
+            }
+        });
+        JButton minus = new JButton("-");
+        minus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                scaler.scale(vv, 1 / 1.1f, vv.getCenter());
+            }
+        });
+
+        JButton reset = new JButton("reset");
+        reset.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT).setToIdentity();
+                vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).setToIdentity();
+            }
+        });
+
+        JPanel controls = new JPanel();
+        controls.add(plus);
+        controls.add(minus);
+        controls.add(reset);
+        frame.getContentPane().add(controls, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
--- src/edu/upc/epsevg/prop/NinePuzzle.java
+++ src/edu/upc/epsevg/prop/NinePuzzle.java
@@ -1,118 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.upc.epsevg.prop;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  *
- * @author Bernat
+ * @author Usuari
  */
 public class NinePuzzle {
-
-    //-------------------------------------------
-    private static int nextId = 0;
-
-    public static void resetIds() {
-        nextId = 0;
+    private static int nextId=0;
+    public static void resetIds(){
+        nextId=2;
     }
-    //-------------------------------------------
-
     private int id;
-    private static final int N = 3;
-    public int[][] puzzle;
-
-    public NinePuzzle(int[] positions) {
-        this.id = nextId++;
-        puzzle = new int[N][N];
-        if (positions.length != N * N) {
-            throw new RuntimeException("Mida de puzzle incorrecta");
-        }
-        for (int i = 0; i < positions.length; i++) {
-            puzzle[i / N][i % N] = positions[i];
-        }
-    }
-
-    NinePuzzle(NinePuzzle p) {
-        puzzle = p.puzzle;
-        this.id = nextId++;
-    }
-
+    private int depth=0;
+    
     public int getId() {
         return id;
     }
 
-    public boolean isSolucio(NinePuzzle solucio) {
-        return Arrays.deepEquals(this.puzzle, solucio.puzzle);
+    public int getDepth() {
+        return depth;
     }
 
-    private int[] find0() {
-        int[] posicio = new int[2];
-        boolean trobat = false;
-        for (int i = 0; i < 3 && !trobat; i++) {
-            for (int j = 0; j < 3 && !trobat; j++) {
-                if (this.puzzle[i][j] == 0) {
-                    posicio[0] = j;
-                    posicio[1] = i;
-                    trobat = true;
-                }
+    private static final int N=3;
+    public static final int EMPTY = -1;
+    private int emptyF, emptyC;
+    public int[][] puzzle;
+    
+    public NinePuzzle(int[] positions){
+        this.id = ++nextId;
+        puzzle = new int[N][N];      
+        if(positions.length!=N*N) throw new RuntimeException("Mida de puzzle incorrecta");
+        for(int i=0;i<positions.length;i++) {
+            puzzle[i/N][i%N]=positions[i];
+            if(positions[i]<1||positions[i]>8) {
+                emptyF = i/N;
+                emptyC = i%N;
             }
         }
-        return posicio;
+    }
+    public NinePuzzle(NinePuzzle other){
+        this.id = ++nextId;
+        puzzle = new int[N][N];      
+        emptyF = other.emptyF;
+        emptyC = other.emptyC;
+        depth =  other.depth+1;
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                puzzle[i][j]=other.puzzle[i][j];
+            }
+        }        
     }
 
-    public ArrayList<NinePuzzle> expand() {
-        ArrayList<NinePuzzle> LF = new ArrayList<>();
-        int[] pos = this.find0();
-        int temp;
-        // move up
-        if (pos[1]-1 > -1) {
-            NinePuzzle p = new NinePuzzle(this);
-            p.puzzle[pos[0]][pos[1]] = p.puzzle[pos[0]][pos[1] - 1];
-            p.puzzle[pos[0]][pos[1] - 1] = 0;
-            LF.add(p);
-            System.out.println(p);
-        }
-        // move down
-        if (pos[1]+1 < 3) {
-            NinePuzzle p = new NinePuzzle(this);
-            p.puzzle[pos[0]][pos[1]] = p.puzzle[pos[0]][pos[1] + 1];
-            p.puzzle[pos[0]][pos[1] + 1] = 0;
-            LF.add(p);
-            System.out.println(p);
 
+    
+    public List<Dir> validMoves() {
+        ArrayList<Dir> moves = new ArrayList<>();
+        for(Dir d:Dir.values()) {
+         if(checkPos(emptyF+d.df, emptyC+d.dc)){
+             moves.add(d);
+         }
         }
-        // move left
-        if (pos[0]-1 > -1) {
-            NinePuzzle p = new NinePuzzle(this);
-            p.puzzle[pos[0]][pos[1]] = p.puzzle[pos[0] - 1][pos[1]];
-            p.puzzle[pos[0] - 1][pos[1]] = 0;
-            LF.add(p);
-            System.out.println(p);
+        return moves;
+    }
+    
+    public void move(Dir d){
+        move(emptyF, emptyC, d);
+    }
+    
+    private void move(int f, int c, Dir d){
+        if(!checkPos(f+d.df,c+d.dc))throw new RuntimeException("Posició incorrecta"+(f+d.df)+","+(c+d.dc));
+        swap( f,c, f+d.df, c+d.dc);
+        emptyF = f+d.df;
+        emptyC = c+d.dc;
+    }
 
+    private void swap(int f1, int c1, int f2, int c2) {
+        int tmp = puzzle[f1][c1];
+        puzzle[f1][c1] = puzzle[f2][c2];
+        puzzle[f2][c2] = tmp;
+    }
+
+    private boolean checkPos(int f, int c) {
+        return (f>=0 && c>=0 && f<N && c<N);
+    }
+    
+    public boolean isSolution(NinePuzzle other){
+        
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                if(puzzle[i][j]!=other.puzzle[i][j]) return false;
+            }
         }
-        // move right
-        if (pos[0]+1 < 3) {
-            NinePuzzle p = new NinePuzzle(this);
-            p.puzzle[pos[0]][pos[1]] = p.puzzle[pos[0] + 1][pos[1]];
-            p.puzzle[pos[0] + 1][pos[1]] = 0;
-            LF.add(p);
-            System.out.println(p);
-        }
-        return LF;
+        return true;
     }
 
     @Override
     public String toString() {
-        String s = "<html>id=" + id + "<br/>\n";
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s += puzzle[i][j] + ",";
+        String s =  "<html>id=" + id + "<br/>\n" ;
+         for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                s += puzzle[i][j]+",";
             }
             s += "<br/>\n";
         }
-        s += "</html>";
+         s+="</html>";
         return s;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.id;
+        return hash;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final NinePuzzle other = (NinePuzzle) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }    
+
+    public NinePuzzle clone() {
+        return new NinePuzzle(this);   
+    }
 }
